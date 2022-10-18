fb0a7b02384b057b8b53dac8263f54d8c689f171
Javier Bonet <javbonet@gmail.com>
2022-10-17 21:48:29 -0300

    Fixes login/logout wrong behavior (cont)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 import { useAppContext } from './lib/context';
 import Header from './components/commons/Header';
 
-function App() {
+interface PropsInterface {
+  user: LocalStorageUser | undefined;
+  logout: () => void;
+}
+
+function App({ user, logout }: PropsInterface) {
   return (
     <>
-      <Header />
+      <Header user={user} logout={logout} />
       <main className="general-container">
         <h1>Medical appointments web site</h1>
       </main>
